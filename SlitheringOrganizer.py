a308a62741fbdcd6832c16d315426bb3a45f5829
Sushant Rao <sushdude34@gmail.com>
2017-07-05 01:15:57 +0530

    Now reads direc/extensions from text file
    Reads directories and extensions dictionary from file

--- SlitheringOrganizer.py
+++ SlitheringOrganizer.py
@@ -1,19 +1,10 @@
 import os
+import json
+import pickle
 #C:\Users\Sushant\Dropbox\College\Summer 17\Python\SlitheringMover
 
-#extract extensions from an external file
-#extract directories from an external file
 #figure out how to organize college lecture pdf and ppt
 
-#directory's and exclusions
-downloads_dir= "C:\Users\Sushant\Downloads";
-images_dir="C:\Users\Sushant\Downloads\Slither_organized\Pictures\\";
-setup_dir="C:\Users\Sushant\Downloads\Slither_organized\Setup Files\\";
-music_dir="C:\Users\Sushant\Downloads\Slither_organized\Music\\";
-video_dir="C:\Users\Sushant\Downloads\Slither_organized\Videos\\";
-college_dir="C:\Users\Sushant\Dropbox\College\To_be_organized\\";
-folders_dir="C:\Users\Sushant\Downloads\Slither_organized\Folders\\";
-others_dir="C:\Users\Sushant\Downloads\Slither_organized\Other\\"
 #exclude when slithering
 exclude=["Slither_organized","Folders","Downloadsdesktop.ini","desktop.ini"]
 
@@ -46,20 +37,50 @@ def no_extension(file_dic):
 def moveto(og,newdir):
 	for file,ogfile_loc in og.iteritems():
 		try:
+			#print ogfile_loc
+			#print "\n"
+			#print newdir+file
 			os.rename(ogfile_loc,newdir+file)
 		except WindowsError:
 			pos=file.find('.')
 			newfile=file[:pos]+"_dupl"+file[pos:]
 			os.rename(ogfile_loc,newdir+newfile)
 
-def main_func():
-	down_dic=getallfiles(downloads_dir)
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+def organize():
+	down_dir=""
+	others_dir=""
+	fromfiles = {}
+	#open the file
+	#fromfiles= load_obj("direc_and_ext")
+	with open("direc_and_ext.txt") as file:
+		fromfiles = json.load(file)
+
+	#get downloads directory
+	for direc,key in fromfiles.iteritems():
+		if key == "downloads":
+			down_dir=direc
+		if key == "others":
+			others_dir=direc 
+	
+	down_dic=getallfiles(down_dir)
 	allfiles={}
-	allfiles[images_dir]=file_extension_split([".png",".jpg",".gif",".tif",".bmp",".jpeg"],down_dic)
-	allfiles[setup_dir]=file_extension_split([".exe",".inf",".msi"],down_dic)
-	allfiles[music_dir]=file_extension_split([".mp3",".wav",".aac",".flac"],down_dic)
-	allfiles[video_dir]=file_extension_split([".mp4",".avi",".flv",".wmv",".mov"],down_dic)
-	allfiles[folders_dir]=no_extension(down_dic)
+	#forming nested dictionary based on extension
+	for direc,key in fromfiles.iteritems():
+		if key == "downloads" or key == "others" :
+			continue 
+		if key == "folders" :
+			allfiles[direc]=no_extension(down_dic)
+		allfiles[direc]=file_extension_split(key,down_dic)
+
+	#moving for most extension
 	for dir_file,ext_dic in allfiles.iteritems():
 		moveto(ext_dic,dir_file) 
 	
+	#for moving others
+	down_dic=getallfiles(down_dir)
+	moveto(down_dic,others_dir)
